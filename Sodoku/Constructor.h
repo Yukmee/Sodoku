53c53dfb8aa8de76c36b38e2436bdd441327e48b
Qasa INto <m78@m78dexingjirenshidemacbook-pro.local>
2018-04-09 21:47:31 +0800

    Now you can use options in Terminal.

---
+++ Sodoku/Constructor.h
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+void construct(const char *string) {
+    printf("I am the constructor.\n");
+    
+    
+}
--- Sodoku/main.cpp
+++ Sodoku/main.cpp
@@ -1,8 +1,21 @@
-#include "Puzzle.h"
+//#include "Puzzle.h"
 #include <iostream>
-using namespace std;
 
-int main(int argc, const char * argv[]) {    
-    cout << "Hello, World!\n";
+#include "Solution.h"
+#include "Constructor.h"
+
+
+int main(int argc, const char * argv[]) {
+    
+    fopen("sudoku.txt", "w");
+
+    if (strcmp(argv[1], "-c")  == 0) {
+        
+        construct(argv[2]);
+
+    } else if (strcmp(argv[1], "-s")  == 0) {
+        solve();
+    }
+    
     return 0;
 }
