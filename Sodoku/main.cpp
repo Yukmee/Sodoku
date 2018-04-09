e34e6a2ce37367ce5c43f092c1f7358e866b0273
Qasa INto <m78@m78dexingjirenshidemacbook-pro.local>
2018-04-09 18:33:56 +0800

    Initial Commit

---
+++ Sodoku/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Sodoku
+//
+//  Created by M78的星际人士 on 4/9/18.
+//  Copyright © 2018 M78的星际人士. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
